45f0b43c004c829f9472cc4fc9a47e4418f94bcc
Alifa Stith <alifastith@gmail.com>
2021-06-13 05:28:32 -0400

    Some signup activity code, setting up User DB nodes

--- app/src/main/java/com/example/dogwalker/SignUpActivity.java
+++ app/src/main/java/com/example/dogwalker/SignUpActivity.java
@@ -3,12 +3,34 @@ package com.example.dogwalker;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class SignUpActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    private FirebaseUser currentUser;
+    private EditText profileName, phoneNumber, emailAddress, password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+        currentUser = mAuth.getCurrentUser();
+
+        profileName = findViewById(R.id.profile_name);
+        phoneNumber = findViewById(R.id.phone_number);
+        emailAddress = findViewById(R.id.email_address);
+        password = findViewById(R.id.password);
+    }
+
+    public void signUp(View view) {
+
     }
 }
