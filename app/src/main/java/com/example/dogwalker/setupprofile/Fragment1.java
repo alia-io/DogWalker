444cd97570efe5be631a2b1ce792d99b827b3a52
Alifa Stith <alifastith@gmail.com>
2021-06-15 00:21:05 -0400

    first fragment layout fixes

--- app/src/main/java/com/example/dogwalker/setupprofile/Fragment1.java
+++ app/src/main/java/com/example/dogwalker/setupprofile/Fragment1.java
@@ -9,8 +9,10 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.NumberPicker;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.constraintlayout.widget.ConstraintLayout;
@@ -26,16 +28,21 @@ public class Fragment1 extends Fragment {
     private int dogWalkerTimeId;
     private CheckBox dogOwner;
     private CheckBox dogWalker;
+    private TextView dogOwnerText;
+    private TextView dogWalkerText;
     private LinearLayout dogOwnerTime;
     private LinearLayout dogWalkerTime;
-    private NumberPicker dogOwnerYears;
-    private NumberPicker dogOwnerMonths;
-    private NumberPicker dogOwnerDays;
-    private NumberPicker dogWalkerYears;
-    private NumberPicker dogWalkerMonths;
-    private NumberPicker dogWalkerDays;
-    private ConstraintLayout parentLayout;
-    private ConstraintSet constraintSet;
+    private EditText dogOwnerYears;
+    private EditText dogOwnerMonths;
+    private EditText dogOwnerDays;
+    private EditText dogWalkerYears;
+    private EditText dogWalkerMonths;
+    private EditText dogWalkerDays;
+    private LinearLayout parentLayout;
+    private LinearLayout dogOwnerLayout;
+    private LinearLayout dogWalkerLayout;
+    //private ConstraintLayout parentLayout;
+    //private ConstraintSet constraintSet;
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -48,8 +55,13 @@ public class Fragment1 extends Fragment {
             dogWalkerTimeId = R.id.dog_walker_time;
 
             parentLayout = view.findViewById(parentId);
+            dogOwnerLayout = view.findViewById(R.id.dog_owner_layout);
+            dogWalkerLayout = view.findViewById(R.id.dog_walker_layout);
+
             dogOwner = view.findViewById(R.id.dog_owner);
             dogWalker = view.findViewById(dogWalkerId);
+            dogOwnerText = view.findViewById(R.id.owner_instruction);
+            dogWalkerText = view.findViewById(R.id.walker_instruction);
             dogOwnerTime = view.findViewById(R.id.dog_owner_time);
             dogWalkerTime = view.findViewById(dogWalkerTimeId);
 
@@ -60,43 +72,39 @@ public class Fragment1 extends Fragment {
             dogWalkerMonths = view.findViewById(R.id.walker_months);
             dogWalkerDays = view.findViewById(R.id.walker_days);
 
-            dogOwnerYears.setMinValue(0);
-            dogOwnerYears.setMaxValue(99);
-            dogOwnerMonths.setMinValue(0);
-            dogOwnerMonths.setMaxValue(11);
-            dogOwnerDays.setMinValue(0);
-            dogOwnerDays.setMaxValue(30);
-
-            dogWalkerYears.setMinValue(0);
-            dogWalkerYears.setMaxValue(99);
-            dogWalkerMonths.setMinValue(0);
-            dogWalkerMonths.setMaxValue(11);
-            dogWalkerDays.setMinValue(0);
-            dogWalkerDays.setMaxValue(30);
-
-            constraintSet = new ConstraintSet();
-            constraintSet.clone(parentLayout);
+            //constraintSet = new ConstraintSet();
+            //constraintSet.clone(parentLayout);
 
             dogOwner.setOnCheckedChangeListener((buttonView, isChecked) -> {
                 if (isChecked) {
+                    dogOwnerText.setVisibility(View.VISIBLE);
                     dogOwnerTime.setVisibility(View.VISIBLE);
-                    constraintSet.connect(dogWalkerId, ConstraintSet.TOP, R.id.line_75, ConstraintSet.TOP);
+                    //parentLayout.setWeightSum(parentLayout.getWeightSum() + 1f);
+
+                    /*constraintSet.connect(dogWalkerId, ConstraintSet.TOP, R.id.line_75, ConstraintSet.TOP);
                     constraintSet.connect(dogWalkerId, ConstraintSet.BOTTOM, R.id.line_85, ConstraintSet.BOTTOM);
                     constraintSet.connect(dogWalkerTimeId, ConstraintSet.TOP, R.id.line_90, ConstraintSet.TOP);
-                    constraintSet.connect(dogWalkerTimeId, ConstraintSet.BOTTOM, parentId, ConstraintSet.BOTTOM);
+                    constraintSet.connect(dogWalkerTimeId, ConstraintSet.BOTTOM, parentId, ConstraintSet.BOTTOM);*/
                 } else {
+                    dogOwnerText.setVisibility(View.GONE);
                     dogOwnerTime.setVisibility(View.GONE);
-                    constraintSet.connect(dogWalkerId, ConstraintSet.TOP, R.id.line_60, ConstraintSet.TOP);
+                    /*constraintSet.connect(dogWalkerId, ConstraintSet.TOP, R.id.line_60, ConstraintSet.TOP);
                     constraintSet.connect(dogWalkerId, ConstraintSet.BOTTOM, R.id.line_70, ConstraintSet.BOTTOM);
                     constraintSet.connect(dogWalkerTimeId, ConstraintSet.TOP, R.id.line_75, ConstraintSet.TOP);
-                    constraintSet.connect(dogWalkerTimeId, ConstraintSet.BOTTOM, R.id.line_85, ConstraintSet.BOTTOM);
+                    constraintSet.connect(dogWalkerTimeId, ConstraintSet.BOTTOM, R.id.line_85, ConstraintSet.BOTTOM);*/
                 }
-                constraintSet.applyTo(parentLayout);
+                //constraintSet.applyTo(parentLayout);
             });
 
             dogWalker.setOnCheckedChangeListener((buttonView, isChecked) -> {
-                if (isChecked) dogWalkerTime.setVisibility(View.VISIBLE);
-                else dogWalkerTime.setVisibility(View.GONE);
+                if (isChecked) {
+                    dogWalkerText.setVisibility(View.VISIBLE);
+                    dogWalkerTime.setVisibility(View.VISIBLE);
+                }
+                else {
+                    dogWalkerText.setVisibility(View.GONE);
+                    dogWalkerTime.setVisibility(View.GONE);
+                }
             });
         }
 
@@ -117,7 +125,8 @@ public class Fragment1 extends Fragment {
     public void onDetach() {
         super.onDetach();
         fragmentTracker.saveFragment1(dogOwner.isChecked(), dogWalker.isChecked(),
-                dogOwnerYears.getValue(), dogOwnerMonths.getValue(), dogOwnerDays.getValue(),
-                dogWalkerYears.getValue(), dogWalkerMonths.getValue(), dogWalkerDays.getValue());
+                Integer.parseInt(dogOwnerYears.getText().toString()), Integer.parseInt(dogOwnerMonths.getText().toString()),
+                Integer.parseInt(dogOwnerDays.getText().toString()), Integer.parseInt(dogWalkerYears.getText().toString()),
+                Integer.parseInt(dogWalkerMonths.getText().toString()), Integer.parseInt(dogWalkerDays.getText().toString()));
     }
 }
