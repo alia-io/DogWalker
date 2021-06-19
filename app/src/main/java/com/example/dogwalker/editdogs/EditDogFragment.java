462ac88a0c0d9e21c385b9f8267abf62749c18e2
Alifa Stith <alifastith@gmail.com>
2021-06-19 02:06:21 -0400

    add/edit dog fragment layout

--- app/src/main/java/com/example/dogwalker/editdogs/EditDogFragment.java
+++ app/src/main/java/com/example/dogwalker/editdogs/EditDogFragment.java
@@ -1,34 +1,68 @@
 package com.example.dogwalker.editdogs;
 
+import com.example.dogwalker.R;
+
+import android.app.ActionBar;
 import android.app.Dialog;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.ArrayAdapter;
+import android.widget.FrameLayout;
+import android.widget.Spinner;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.DialogFragment;
 
 public class EditDogFragment extends DialogFragment {
 
-    public static EditDogFragment newInstance(int layout) {
+    private View view;
+    private int height = 0;
+
+    public static EditDogFragment newInstance(int layout, int height) {
         EditDogFragment fragment = new EditDogFragment();
         Bundle args = new Bundle();
         args.putInt("layout", layout);
+        args.putInt("height", height);
         fragment.setArguments(args);
         return fragment;
     }
 
     @Override
+    public void onResume() {
+        super.onResume();
+        ViewGroup.LayoutParams params = getDialog().getWindow().getAttributes();
+        params.height = height;
+        getDialog().getWindow().setAttributes((WindowManager.LayoutParams) params);
+    }
+
+    @NonNull @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
 
         Bundle args = getArguments();
         int layout = args.getInt("layout");
+        height = args.getInt("height");
 
         // TODO: getTag() == "add_dog" || "edit_dog"
 
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
         LayoutInflater inflater = getActivity().getLayoutInflater();
-        View view = inflater.inflate(layout, null);
+        view = inflater.inflate(layout, null);
+
+        Spinner spinner = view.findViewById(R.id.walk_length_units);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getContext(),
+                R.array.dog_walk_length_units, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
+        // button onClick
+        //RadioGroup rg = findViewById(R.id.rg);
+        //rg.getCheckedRadioButtonId();
+        //switch to find checked rb
 
         return builder.setView(view).create();
     }
--- app/src/main/java/com/example/dogwalker/editdogs/EditDogsActivity.java
+++ app/src/main/java/com/example/dogwalker/editdogs/EditDogsActivity.java
@@ -3,19 +3,29 @@ package com.example.dogwalker.editdogs;
 import com.example.dogwalker.R;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 public class EditDogsActivity extends AppCompatActivity {
+
+    ConstraintLayout activityLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_dogs);
+        setSupportActionBar(findViewById(R.id.toolbar));
+
+        activityLayout = findViewById(R.id.layout_parent);
     }
 
     public void addDog(View view) {
-        EditDogFragment.newInstance(R.layout.fragment_edit_dog).show(getSupportFragmentManager(), "add_dog");
+        Log.d("height", "view height = " + activityLayout.getHeight());
+        Log.d("height", "subtract height = " + getSupportActionBar().getHeight());
+        EditDogFragment.newInstance(R.layout.fragment_edit_dog, activityLayout.getHeight() - (getSupportActionBar().getHeight() * 3))
+                .show(getSupportFragmentManager(), "add_dog");
     }
 }
